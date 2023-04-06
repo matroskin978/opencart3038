c9fd881b45b2f981dbbe72053d998e2a991e3dec
Админ <matroskin8@i.ua>
2023-04-06 16:47:31 +0300

    Lesson 6

---
+++ system/storage/modification/system/library/template/twig.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Template;
+
+final class Twig
+{
+	private $data = array();
+
+	public function set($key, $value)
+	{
+		$this->data[$key] = $value;
+	}
+
+	public function render($filename, $code = '')
+	{
+		if (!$code) {
+			$file = DIR_TEMPLATE . $filename . '.twig';
+
+			if (defined('DIR_CATALOG') && is_file(DIR_MODIFICATION . 'admin/view/template/' . $filename . '.twig')) {	
+                $code = file_get_contents(DIR_MODIFICATION . 'admin/view/template/' . $filename . '.twig');
+            } elseif (is_file(DIR_MODIFICATION . 'catalog/view/theme/' . $filename . '.twig')) {
+                $code = file_get_contents(DIR_MODIFICATION . 'catalog/view/theme/' . $filename . '.twig');
+            } elseif (is_file($file)) {
+				$code = file_get_contents($file);
+			} else {
+				throw new \Exception('Error: Could not load template ' . $file . '!');
+				exit();
+			}
+		}
+
+		// initialize Twig environment
+		$config = array(
+			'autoescape'  => false,
+			'debug'       => true,
+			'auto_reload' => true,
+			'cache'       => DIR_CACHE . 'template/'
+		);
+
+		try {
+			$loader = new \Twig\Loader\ArrayLoader(array($filename . '.twig' => $code));
+
+			$twig = new \Twig\Environment($loader, $config);
+			$twig->addExtension(new \Twig\Extension\DebugExtension);
+
+			return $twig->render($filename . '.twig', $this->data);
+		} catch (Exception $e) {
+			trigger_error('Error: Could not load template ' . $filename . '!');
+			exit();
+		}
+	}
+}
